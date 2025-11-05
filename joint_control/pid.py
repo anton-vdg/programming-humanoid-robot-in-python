d8f562536fbca550d38b117bef16615a81d3b1ad
Anton Dilg <anton.dilg@hhi.fraunhofer.de>
2025-11-05 11:45:56 +0100

    first version of angle_interpolation.py: it works for almost all the keyframes except for BellyToStand()

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 8.0
+        self.Ki = 0.20
+        self.Kd = 0.20
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,6 +53,12 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
+        e = target - sensor       
+        self.u +=  (self.Kp + self.Ki * self.dt + self.Kd / self.dt) * e - (self.Kp +(2 * self.Kd) / self.dt) * self.e1 + (self.Kd / self.dt) * self.e2
+        self.e2 = self.e1.copy()
+        self.e1 = e.copy()
+        pred = self.u + ((self.u - sensor) + (self.y.popleft() - sensor)) / (2 * self.dt) * self.dt
+        self.y.append(pred)
 
         return self.u
 
